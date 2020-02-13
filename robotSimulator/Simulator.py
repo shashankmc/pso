302d6a8e37d8005a6ed261f89ade584623a874fb
Shashank <shashank.chakravarthy@gmail.com>
2020-02-13 01:42:39 +0100

    Adding key inputs to move the object around by refreshing the entire frame

--- robotSimulator/Simulator.py
+++ robotSimulator/Simulator.py
@@ -1,5 +1,6 @@
 import time
 from pynput.keyboard import Key, Listener
+from pygame.locals import *
 import pygame
 import random
 
@@ -11,7 +12,10 @@ timeTick = 0
 tickRate = 13.0
 robot: Robot
 obstacleList = []
-
+# initiates pygame for the simulation of an object
+pygame.init()
+# set the screen size as per requirement
+screen = pygame.display.set_mode((640, 480))
 
 def init():
     global robot
@@ -45,33 +49,75 @@ def display():
 
 # this function is to display the frame for the simulation
 def initDisplay():
-    # initiates pygame for the simulation of an object
-    pygame.init()
-    # set the screen size as per requirement
-    screen = pygame.display.set_mode((640, 480))
     # empty the background -- this needs to be set only initially. This is here for current run
     background = pygame.Surface(screen.get_size())
     # make the background white in color
     background.fill((255, 255, 255))
     # convert the background with the provided settings and display with the necessary objects
     background.convert()
+    x = 25
+    y = 25
     # the circle created will represent the object or robot which will move based on key inputs
     # the parameters for drawing a circle are these - (Surface, color, pos, radius, width=0)
-    pygame.draw.circle(background, (0, 200, 0), (25,25), 25)
+    pygame.draw.circle(background, (0, 200, 0), (x,y), 25)
     # the function blit creates the initially drawing based on the settings
     screen.blit(background, (0, 0))
     waitForInput = True
     FPS = 30
     # once the display is put up in front of the user
     # pygame module waits for key strokes from the user
+    x_change = 0
+    y_change = 0
     while waitForInput:
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 waitForInput = False
-            elif event.type == pygame.K_q:
-                waitForInput = False                
-        pygame.display.flip()
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_q:
+                    waitForInput = False
+                elif event.key == K_d:
+                    print('You just pressed d')
+                    x_change = 25
+                    #x_change = calcBounds(x, x_change, 'x')
+                    x += x_change
+                elif event.key == K_w:
+                    print('You just pressed w')
+                    y_change = -25
+                    # y_change = calcBounds(y, y_change, 'y')
+                    y += y_change
+                elif event.key == K_a:
+                    print('You just pressed a')
+                    x_change = -25
+                    #x_change = calcBounds(x, x_change, 'x')
+                    x += x_change
+                elif event.key == K_s:
+                    print('You have pressed s')
+                    y_change = 25
+                    # y_change = calcBounds(y, y_change, 'y')
+                    y += y_change
+                print(x, y)
+                move(x, y, background)
 
+def move(x, y, background):
+    screen.fill((255, 255, 255))
+    # background.clamp_ip(screen)
+    screen.blit(background, (x,y))
+
+# def calcBounds(pos, pos_change, axes):
+#    if (axes == 'x'):
+#        if(pos - pos_change < 0 or pos + pos_change > 600):
+#            pos_change = 0
+#        else:
+#           pos_change
+#   elif (axes == 'y'):
+#       if(pos_change - pos < 0 or pos + pos_change > 480):
+#           pos_change = 0
+#       else:
+#            pos_change
+#    return (pos_change)
+
+    
 def calculateNextPosition():
     checkCollision()
     print("calc next position")
@@ -111,6 +157,7 @@ def on_release(key):
     if key == Key.esc:
         keepRunning = False
         return False
+
 
 
 initDisplay()
