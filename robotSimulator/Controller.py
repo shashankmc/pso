bda3c7b67ea17651861e54c3d625d3cc94ea3d49
Yannick <y.ruppenthal@student.maastrichtuniversity.nl>
2020-02-23 09:55:15 +0100

    updated controller

--- robotSimulator/Controller.py
+++ robotSimulator/Controller.py
@@ -3,31 +3,82 @@ import numpy as np
 
 class Controller:
 
-    weights: []
     layersSizes: []
     bias = 1
+    populationSize = 1
+    population : np.array
 
-    def __init__(self, layers: []):
-
+    def __init__(self, layers: [], populationSize):
         print("init: layers: " + str(layers))
-        self.weights = []
-        for layerLevel in range(1, len(layers)):
-            self.weights.append(np.random.rand(layers[layerLevel - 1] + self.bias, layers[layerLevel]))
+        self.population = []
+        #we need a population of networks
+        for ps in range(0, populationSize):
+            weights = []
+            #networklayers
+            for layerLevel in range(1, len(layers)):
+                weights.append(np.random.rand(layers[layerLevel - 1] + self.bias, layers[layerLevel]))
+            self.population.append(weights)
+
+
 
     def sigmoid(self, z):
         return 1 / (1 + np.exp(-z))
 
     def calc(self, inputValues: []):
+        resultAll = []
+        for weights in self.population:
+            #too lazy to look up how to throw an exception ^^
+            if len(inputValues) != len(weights[0]) - self.bias:
+                return -1
 
-        if len(inputValues) != len(self.weights[0]) - self.bias:
-            print("inpL: " + str(len(inputValues)) + "")
-            return -1
+            #adds a bias to the input array
+            res = np.append(np.array(inputValues), self.bias)
 
-        res = np.append(np.array(inputValues), self.bias)
+            #iterates through the the wheigt matrices connecting layer n and n-1
+            for layerWeight in weights:
+                res = np.matmul(res, layerWeight)
+                res = self.sigmoid(res)
+                res = np.append(res, self.bias)
 
-        for layerWeight in self.weights:
-            res = np.matmul(res, layerWeight)
-            res = self.sigmoid(res)
-            res = np.append(res, self.bias)
+            #adds the result vector of n-th network to resultAll
+            #removes the last entry as it is only the bias
+            resultAll.append(res[0:-1])
 
-        return res[0:-1]
+        #resultVector of every network
+        return resultAll
+
+    #updates the weight matrix of connecting layerLevel and layerLevel -1
+    def train(self, layerLevel):
+
+        #position is index of the network, and value is fitness score
+        evaluated = []
+        for nwWeights in self.population:
+            evaluated.append(self.fit(nwWeights))
+
+        #create a selection with the evaluated list and self.population
+        selection = self.selection(evaluated)
+
+        #reproduces a new population with the selected population
+        reproduction = self.reproduction(selection)
+
+        #cross mutation because maybe we create spiderman
+        crossMutation = self.crossMutation(reproduction)
+
+        #update the weights
+        self.population = crossMutation
+
+    def crossMutation(self, reproducedNW):
+        print("wait and weight sounds similar")
+        return reproducedNW
+
+    def reproducation(self, selectedNW):
+        print("f!#$ you")
+        return selectedNW
+
+    def selection(self, evalNW):
+        print("neither do it")
+        return evalNW
+
+    def fit(self, network):
+        print("I don t work")
+        return network
